afd6cc6e4ed8f866f233a6b5f0757bf6b2b1f635
Affan Dzaki Abrar <affandzaki15@gmail.com>
2026-09-06 22:01:39 +0700

    js stats

--- JS/web1.js
+++ JS/web1.js
@@ -28,3 +28,30 @@ const students = [
     nilai: 72
   }
 ];
+
+// banyak siswa
+const totalMahasiswa = students.length
+document.querySelector("#total-mahasiswa").textContent = totalMahasiswa
+
+// rata
+function totalRata (){
+  return students.reduce((acc, value)=>{
+    return acc += value.nilai
+  }, 0)
+}
+const hasil1 = totalRata() / students.length
+document.querySelector("#rata-rata").textContent = hasil1
+
+// lulus
+const mahasiswaLulus = students.filter(data => data.nilai >= 80).length
+document.querySelector("#mahasiswa-lulus").textContent = mahasiswaLulus
+
+// nilai tertinggi
+const nilaiTertinggi = students.reduce((acc, value) =>{
+  if(acc.nilai < value.nilai){
+    return value
+  } 
+  return acc
+}, students [0])
+
+document.querySelector("#nilai-tertinggi").textContent = nilaiTertinggi.nilai
