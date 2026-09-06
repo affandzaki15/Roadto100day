95a3830d374d10158c359749d89b959c4bfbd517
Affan Dzaki Abrar <affandzaki15@gmail.com>
2026-09-06 18:43:36 +0700

    html done

---
+++ JS/web1.js
@@ -0,0 +1,30 @@
+const students = [
+  {
+    id: 1,
+    nama: "Affan",
+    umur: 23,
+    jurusan: "Informatika",
+    nilai: 85
+  },
+  {
+    id: 2,
+    nama: "Budi",
+    umur: 22,
+    jurusan: "Sistem Informasi",
+    nilai: 65
+  },
+  {
+    id: 3,
+    nama: "Caca",
+    umur: 21,
+    jurusan: "Informatika",
+    nilai: 90
+  },
+  {
+    id: 4,
+    nama: "Dina",
+    umur: 22,
+    jurusan: "Teknik Komputer",
+    nilai: 72
+  }
+];
