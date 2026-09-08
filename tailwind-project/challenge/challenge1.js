5c4eaed572b3c3d04130342a75f3efb23ad5bf6b
Affan Dzaki Abrar <affandzaki15@gmail.com>
2026-09-08 15:03:52 +0700

    install tailwind

---
+++ tailwind-project/challenge/challenge1.js
@@ -0,0 +1,34 @@
+const films = [
+  { id: 1, judul: "Inception", genre: "Sci-Fi", rating: 8.8 },
+  { id: 2, judul: "Parasite", genre: "Drama", rating: 8.5 },
+  { id: 3, judul: "Interstellar", genre: "Sci-Fi", rating: 8.7 },
+  { id: 4, judul: "Whiplash", genre: "Drama", rating: 8.5 },
+];
+
+const ambil = films
+    .filter(data => data.rating >= 8.7)
+    .map(data => data.judul)
+    console.log(ambil)
+
+const vehicles = [
+  { nama: "Avanza", jenis: "MPV", harga: 250 },
+  { nama: "Civic", jenis: "Sedan", harga: 500 },
+  { nama: "Xpander", jenis: "MPV", harga: 300 },
+  { nama: "Brio", jenis: "Hatchback", harga: 200 },
+];
+const harga = vehicles
+    .find(data => data.harga <=300)
+    console.log(harga.nama)
+
+const games = [
+  { nama: "Elden Ring", genre: "RPG", harga: 600, stok: 3 },
+  { nama: "FIFA 26", genre: "Sport", harga: 500, stok: 5 },
+  { nama: "Minecraft", genre: "Sandbox", harga: 300, stok: 8 },
+  { nama: "Cyberpunk 2077", genre: "RPG", harga: 450, stok: 2 },
+];
+
+const totalGame = games.reduce((acc, value)=>{
+    return acc += value.harga * value.stok
+}, 0)
+const hasil = `stok semuanya adalah ${totalGame}`
+console.log(hasil)
